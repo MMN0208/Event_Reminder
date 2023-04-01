fbd7e82e7cb29bd5f17a607b1bf2ed40d036862e
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2023-04-01 17:19:55 +0700

    Redone semester + ay regex + miscellaneous fixes

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -7,18 +7,19 @@ function parseCalendarInput(input) {
     const lineCount = line.length;
     let classList = [];
 
-    let year = '2023';
+    let semester = null;
+    let ay = [];
 
     for (let i = 0; i < lineCount; i++) {
         let tokenizedLine = line[i].split('\t'); // each token is separated by \t (tab)
 
         if (tokenizedLine.length === 1) {
-            let semester_ay = tokenizedLine[0].trim().match(/HK([1-3])\s\((20[1-9][1-9])-(20[1-9][1-9])\)/);
+            let semester_ay = tokenizedLine[0].trim().match(/HK([1-3])\s\((20[0-9][0-9])-(20[0-9][0-9])\)/);
             if(semester_ay != null) {
-                let semester = semester_ay[1].trim();
-                
-                year = semester === "1" ? semester_ay[2].trim() : semester_ay[3].trim();
-                console.log(year);
+                semester = semester_ay[1].trim();
+                ay = [];
+                ay.push(semester_ay[2].trim());
+                ay.push(semester_ay[3].trim());
             }
             else continue;
         }
@@ -39,6 +40,12 @@ function parseCalendarInput(input) {
     
             if (day !== '' && month !== '' && time !== '' && room !== '--') {
                 let type = 0;
+                let year = new Date().getFullYear();
+
+                if (semester != null) {
+                    year = (semester === '1' && parseInt(month) >= 8) ?  ay[0] : ay[1];
+                }
+                
                 classList.push({
                     signature: Base64.stringify(sha256(ID + day + month + year + time.join('') + room)),
                     ID,
@@ -64,6 +71,12 @@ function parseCalendarInput(input) {
     
             if (day !== '' && month !== '' && time !== '' && room !== '--') {
                 let type = 1;
+                let year = new Date().getFullYear();
+
+                if (semester != null) {
+                    year = (semester === '1' && parseInt(month) >= 8) ?  ay[0] : ay[1];
+                }
+
                 classList.push({
                     signature: Base64.stringify(sha256(ID + day + month + year + time.join('') + room)),
                     ID,
@@ -79,9 +92,9 @@ function parseCalendarInput(input) {
                 });
             }
         }
-
         else continue;
     }
+    console.log(classList);
     return classList;
 }
 
