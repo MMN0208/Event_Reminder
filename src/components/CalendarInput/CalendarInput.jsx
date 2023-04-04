4d037b4c38923dfd12e1b7f3c82fda0bfe84628a
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2023-04-05 00:50:53 +0700

    minor change

--- src/components/CalendarInput/CalendarInput.jsx
+++ src/components/CalendarInput/CalendarInput.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CalendarInput.css';
+
 function CalendarInputField(props) {
     return <textarea rows="5" type='text' value={props.value} onChange={props.onChange} className='calendar-input' placeholder="HK2 (2022-2023) or Học kỳ 2 Năm học 2022 - 2023&#10;Ngày cập nhật:...&#10;MÃ MH..." wrap="soft"></textarea>
 }
