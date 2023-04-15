23a29b52dd2ceb9fd61865ca08e52457594feb92
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2023-04-16 01:29:54 +0700

    Update logo for mobile devices, edit guideline and calendar input field

--- src/components/GuideLine/GuideLine.jsx
+++ src/components/GuideLine/GuideLine.jsx
@@ -3,9 +3,11 @@ import "./GuideLine.css";
 
 function GuideLine(props) {
     return <div className="guide-line_wrapper">
-        <span className="guide-line_number">
-            {props.num}
-        </span>
+        <div className="guide-line_shape">
+            <div className="guide-line_number">
+                {props.num}
+            </div>
+        </div>
         <span className="guide-line_description">{props.text}</span>
     </div>
 }
